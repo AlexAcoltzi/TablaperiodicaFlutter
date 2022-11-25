6c9b9e547893e6d4df49b2ceafe9809e0b4dee9f
alexf <alex.fantastico22@gmail.com>
2022-11-24 18:36:46 -0600

    Agregando los datos de los elementos

---
+++ lib/Data/element.dart
@@ -0,0 +1,8 @@
+class Element{
+  final int id;
+  final String name;
+  final String simbol;
+  final int atomicNomber;
+
+  Element({required this.id, required this.name, required this.simbol, required this.atomicNomber});
+}
